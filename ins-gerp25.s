8b2eb8dcc608cdf16bab25b1cfa68ca867566cc5
nattfalk <michaelnattfalk@gmail.com>
2024-11-19 21:48:41 +0100

    Add draft for transition effect before sine scroller

--- ins-gerp25.s
+++ ins-gerp25.s
@@ -1,6 +1,7 @@
 	include "include/hardware/custom.i"
 	include "include/blitter.i"
 	include "include/bits.i"
+	include	"include/macros.i"
 	INCLUDE "common/startup.s"
 	
 ********** Flags **************
@@ -153,10 +154,11 @@ DrawBuffer:		dc.l	Screen2
 ViewBuffer:		dc.l	Screen
 
 EffectsTable:		
-			dc.l	2*50, HorizontalStrips_Init, HorizontalStrips_Run, HorizontalStrips_Interrupt
-			dc.l	(6+2)*50, Logo_Init, Logo_Run, Logo_Interrupt
-			dc.l	(9+8)*50, DotRemove_Init, DotRemove_Run, DotRemove_Interrupt
-			dc.l	(20+17)*50, SineScroller_Init, SineScroller_Run, SineScroller_Interrupt
+			; dc.l	2*50, HorizontalStrips_Init, HorizontalStrips_Run, HorizontalStrips_Interrupt
+			; dc.l	(6+2)*50, Logo_Init, Logo_Run, Logo_Interrupt
+			; dc.l	(9+8)*50, DotRemove_Init, DotRemove_Run, DotRemove_Interrupt
+			dc.l	(20+17)*50, TransitionToScroller_Init, TransitionToScroller_Run, TransitionToScroller_Interrupt
+			; dc.l	(20+17)*50, SineScroller_Init, SineScroller_Run, SineScroller_Interrupt
 			; dc.l	15*50, Credits_Init, Credits_Run, Credits_Interrupt
 			; dc.l	20*50, Magnifier_Init, Magnifier_Run, Magnifier_Interrupt
 			; dc.l	19*50, TextLogo_Init, TextLogo_Run, TextLogo_Interrupt
@@ -190,6 +192,7 @@ I			SET		I+40
 	include	"parts/sine_scroller.s"
 	include	"parts/credits.s"
 	include	"parts/logo.s"
+	include	"parts/transition_to_scroller.s"
 	; include "parts/magnifier.s"
 	; include	"parts/textlogo.s"
 	; include "parts/logo.s"
---
+++ parts/transition_to_scroller.s
@@ -0,0 +1,230 @@
+************************************************************
+* Main routines
+************************************************************
+        SECTION TransitionToScroller, CODE_P
+
+		include	"include/macros.i"
+
+FADE_STEPS_UP	=	2
+FADE_STEPS_DOWN	=	16
+FADE_STEPS		=	FADE_STEPS_UP+FADE_STEPS_DOWN
+
+TransitionToScroller_Init:
+		lea.l	$dff000,a6
+
+    	move.l	#TTS_Copper,$80(a6)
+
+		lea.l	TTS_ShadeTable(pc),a0
+		lea.l	TTS_ToColors(pc),a1
+		moveq	#8-1,d7
+.createShades:
+		PUSH	d7
+
+		move.w	(a1)+,d0
+		move.w	TTS_HighLightColor(pc),d1
+		moveq	#FADE_STEPS_UP,d2
+		jsr		CreateShadeTable
+
+		move.w	TTS_HighLightColor(pc),d0
+		move.w	(a1),d1
+		moveq	#FADE_STEPS_DOWN,d2
+		jsr		CreateShadeTable
+
+		POP		d7
+		dbf		d7,.createShades
+
+		rts
+
+TransitionToScroller_Run:
+		lea.l	TTS_FadeTablePtr(pc),a0
+		move.l	(a0),a0
+
+		move.l	(a0),d0
+		cmp.l	#-1,d0
+		beq.s	.done
+		cmp.l	#FADE_STEPS,d0
+		bne.s	.doFade
+		add.l	#18*4,TTS_FadeTablePtr
+		bra.s	TransitionToScroller_Run
+
+.doFade:
+		move.l	d0,d1
+		add.w	d1,d1
+		addq.l	#1,d0
+		move.l	d0,(a0)+
+		move.l	(a0)+,a1
+		move.w	(a1,d1.w),d0
+
+		moveq	#16-1,d7
+.fadeLoop:
+		move.l	(a0)+,a2
+		cmp.l	#-1,a2
+		beq.s	.skip
+
+		move.w	d0,(a2)
+
+.skip:	dbf		d7,.fadeLoop
+
+.done:
+		rts
+
+TransitionToScroller_Interrupt:
+		addq.l	#1,TTS_LocalFrameCounter
+
+		rts
+
+************************************************************
+* Effect routines
+************************************************************
+
+************************************************************
+* Variables and data
+************************************************************
+		even
+TTS_LocalFrameCounter:
+					dc.l	0
+
+TTS_HighLightColor:	dc.w	$0fff
+TTS_ToColors:		dc.w	$0000
+					dc.w	$0fed,$0fda,$0fb6,$0d86,$0978,$0556,$0245,$0134
+
+TTS_FadeTablePtr:	dc.l	TTS_FadeTable
+TTS_FadeTable:		dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*0)
+					dc.l	TTS_COL_00,TTS_COL_01,TTS_COL_10,TTS_COL_11,TTS_COL_20,TTS_COL_21
+					dc.l	TTS_COL_30,TTS_COL_31,TTS_COL_40,TTS_COL_41,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+					
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*1)
+					dc.l	-1,-1,TTS_COL_10,TTS_COL_11,TTS_COL_20,TTS_COL_21
+					dc.l	TTS_COL_30,TTS_COL_31,TTS_COL_40,TTS_COL_41,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*2)
+					dc.l	-1,-1,-1,-1,TTS_COL_20,TTS_COL_21
+					dc.l	TTS_COL_30,TTS_COL_31,TTS_COL_40,TTS_COL_41,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*3)
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	TTS_COL_30,TTS_COL_31,TTS_COL_40,TTS_COL_41,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*4)
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	-1,-1,TTS_COL_40,TTS_COL_41,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*5)
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	-1,-1,-1,-1,TTS_COL_50,TTS_COL_51
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*6)
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	TTS_COL_60,TTS_COL_61,TTS_COL_70,-1
+
+					dc.l	0,TTS_ShadeTable+(FADE_STEPS*2*7)
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	-1,-1,-1,-1,-1,-1
+					dc.l	-1,-1,TTS_COL_70,-1
+
+					dc.l	-1
+
+TTS_ShadeTable:		dcb.w	FADE_STEPS*8
+
+************************************************************
+* Copper
+************************************************************
+        SECTION TTS_Copper, CODE_C
+TTS_Copper:
+		dc.w	$01fc,$0000
+		dc.w	$008e,$2c81
+		dc.w	$0090,$2cc1
+		dc.w	$0092,$0038
+		dc.w	$0094,$00d0
+		dc.w	$0106,$0c00
+		dc.w	$0102,$0003		; 3 pixels offset for text shadow
+		dc.w	$0104,$0000
+		dc.w	$0108,$0000
+		dc.w	$010a,$0000
+		dc.w	$0100,$0000
+
+		dc.w	$0180
+TTS_COL_00:
+		dc.w	$0fed
+
+		dc.b	$8c-($d*6),$01
+		dc.w	$fffe,$0180
+TTS_COL_10:
+		dc.w	$0fda
+
+		dc.b	$8c-($d*5),$01
+		dc.w	$fffe,$0180
+TTS_COL_20:
+		dc.w	$0fb6
+
+		dc.b	$8c-($d*4),$01
+		dc.w	$fffe,$0180
+TTS_COL_30:
+		dc.w	$0d86
+
+		dc.b	$8c-($d*3),$01
+		dc.w	$fffe,$0180
+TTS_COL_40:
+		dc.w	$0978
+
+		dc.b	$8c-($d*2),$01
+		dc.w	$fffe,$0180
+TTS_COL_50:
+		dc.w	$0556
+
+		dc.b	$8c-($d*1),$01
+		dc.w	$fffe,$0180
+TTS_COL_60:
+		dc.w	$0245
+
+		dc.w	$8c01,$fffe
+		dc.w	$0180
+TTS_COL_70:
+		dc.w	$0134
+
+		dc.w	$cc01,$fffe
+		dc.w	$0180
+TTS_COL_61:
+		dc.w	$0245
+		
+		dc.b	$cc+($d*1),$01
+		dc.w	$fffe,$0180
+TTS_COL_51:
+		dc.w	$0556
+		
+		dc.b	$cc+($d*2),$01
+		dc.w	$fffe,$0180
+TTS_COL_41:
+		dc.w	$0978
+		
+		dc.b	$cc+($d*3),$01
+		dc.w	$fffe,$0180
+TTS_COL_31:
+		dc.w	$0d86
+		
+		dc.w	$ffdf,$fffe
+
+		dc.b	$cc+($d*4)-$100,$01
+		dc.w	$fffe,$0180
+TTS_COL_21:
+		dc.w	$0fb6
+
+		dc.b	$cc+($d*5)-$100,$01
+		dc.w	$fffe,$0180
+TTS_COL_11:
+		dc.w	$0fda
+
+		dc.b	$cc+($d*6)-$100,$01
+		dc.w	$fffe,$0180
+TTS_COL_01:
+		dc.w	$0fed
+
+		dc.w	$ffff,$fffe
+		dc.w	$ffff,$fffe
